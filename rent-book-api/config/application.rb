45485d6e966ee0342856be93132b034dd51b12c8
huythanh <huythanh1706@gmail.com>
2020-09-08 12:46:29 +0700

    [add]add I18n

--- rent-book-api/config/application.rb
+++ rent-book-api/config/application.rb
@@ -19,5 +19,9 @@ module RentBookApi
   class Application < Rails::Application
     config.load_defaults 6.0
     config.api_only = true
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
+
   end
 end
