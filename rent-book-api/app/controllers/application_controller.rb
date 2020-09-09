6d6e7e917496265ce8914abf029095d7fb0811a6
duypn-2245 <pham.ngoc.duy@sun-asterisk.com>
2020-09-09 13:13:12 +0700

    [add]add I18n

--- rent-book-api/app/controllers/application_controller.rb
+++ rent-book-api/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  before_action :set_locale
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
+  private
+
+  def set_locale
+    parsed_locale = I18n.available_locales.map(&:to_s).include?(params[:locale]) ? params[:locale] : nil
+    I18n.locale = parsed_locale || I18n.default_locale
+  end
 end
