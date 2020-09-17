bf02c1b2b52c08eb1d1a97407d8d15d925a0deef
huythanh <huythanh1706@gmail.com>
2020-09-17 08:51:25 +0700

    [add] add reject,approve register book

--- rent-book-api/app/models/register_book.rb
+++ rent-book-api/app/models/register_book.rb
@@ -1,5 +1,6 @@
 class RegisterBook < ApplicationRecord
   belongs_to :user
+  has_many :register_book_details, dependent: :destroy
 
   enum status: {pending: 0, approved: 1, rejected: 2, finished: 3, canceled: 4}
   validates :intended_end_date, presence: true
--- rent-book-api/spec/controllers/api/v1/books_controller_spec.rb
+++ rent-book-api/spec/controllers/api/v1/books_controller_spec.rb
@@ -1,5 +1,4 @@
 require "rails_helper"
-require "spec_helper"
 
 RSpec.describe Api::V1::BooksController, type: :controller do
   let(:book){FactoryBot.create(:book)}
